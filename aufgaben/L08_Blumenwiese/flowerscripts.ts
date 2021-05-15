77bb625d3afb9746038de3f65d688533ee2af9df
mirjammu <mirjammu@t-online.de>
2021-05-15 20:51:59 +0200

    L08_Blumenwiese hochgeladen

---
+++ aufgaben/L08_Blumenwiese/flowerscripts.ts
@@ -0,0 +1,224 @@
+namespace Blumenwiese {
+    //inspired by videos from EIA Inverted
+    interface Vector {
+        x: number;
+        y: number;
+    }
+
+    window.addEventListener("load", handleLoad);
+    let crc2: CanvasRenderingContext2D;
+    let golden: number = 0.5;
+    let canvas: HTMLCanvasElement = document.querySelector("canvas")!;
+    canvas.width = 360;
+    canvas.height = 700;
+
+    function handleLoad(): void {
+
+        console.log("ready");
+        let canvas: HTMLCanvasElement | null = document.querySelector("canvas");
+        if (!canvas)
+            return;
+        crc2 = canvas.getContext("2d")!;
+
+        let horizon: number = crc2.canvas.height * golden;
+        let mountainsPosition: Vector = { x: 0, y: horizon };
+
+        drawBackground();
+        drawMountains(mountainsPosition, 75, 200, "grey", "white");
+        drawMountains(mountainsPosition, 50, 150, "grey", "lightgrey");
+        drawCloud({ x: 200, y: 50 }, { x: 100, y: 25 });
+        drawCloud({ x: 50, y: 160 }, { x: 100, y: 25 });
+        drawCloud({ x: 250, y: 150 }, { x: 100, y: 25 });
+        drawSun({ x: 50, y: 50 });
+
+        drawFlowers();
+    }
+
+    function drawBackground(): void {
+        console.log("background");
+
+        let gradient: CanvasGradient = crc2.createLinearGradient(0, 0, 0, crc2.canvas.height);
+        gradient.addColorStop(0, "#467deb");
+        gradient.addColorStop(0.4, "#CCEEFF");
+        gradient.addColorStop(0.5, "lightgreen");
+        gradient.addColorStop(1, "HSL(100, 100%, 28%)");
+
+        crc2.fillStyle = gradient;
+        crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
+
+    }
+
+    function drawMountains(_position: Vector, _min: number, _max: number, _colorLow: string, _colorHigh: string): void {
+        console.log("mountains");
+
+        let stepMin: number = 50;
+        let stepMax: number = 30;
+        let x: number = 0;
+
+        crc2.save();
+        crc2.translate(_position.x, _position.y);
+
+        crc2.beginPath();
+        crc2.moveTo(0, 0);
+        crc2.lineTo(0, -_max);
+
+        do {
+            x += stepMin + Math.random() * (stepMax - stepMin);
+            let y: number = -_min - Math.random() * (_max - _min);
+
+            crc2.lineTo(x, y);
+        } while (x < crc2.canvas.width);
+
+        crc2.lineTo(x, 0);
+        crc2.closePath();
+
+        let gradient: CanvasGradient = crc2.createLinearGradient(0, 0, 0, -_max);
+        gradient.addColorStop(0, _colorLow);
+        gradient.addColorStop(0.7, _colorHigh);
+
+        crc2.fillStyle = gradient;
+        crc2.fill();
+
+        crc2.restore();
+    }
+
+    function drawCloud(_position: Vector, _size: Vector): void {
+        console.log("cloud");
+
+        let nParticles: number = 23;
+        let radiusParticle: number = 15;
+        let particle: Path2D = new Path2D();
+        let gradient: CanvasGradient = crc2.createRadialGradient(0, 0, 0, 0, 0, radiusParticle);
+
+        particle.arc(0, 0, radiusParticle, 0, 2 * Math.PI);
+        gradient.addColorStop(0, "HSLA(0, 50%, 100%, 0.5)");
+        gradient.addColorStop(1, "HSLA(0, 60%, 100%, 0)");
+
+        crc2.save();
+        crc2.translate(_position.x, _position.y);
+        crc2.fillStyle = gradient;
+
+        for (let drawn: number = 0; drawn < nParticles; drawn++) {
+            crc2.save();
+            let x: number = (Math.random() - 0.5) * _size.x;
+            let y: number = - (Math.random() * _size.y);
+            crc2.translate(x, y);
+            crc2.fill(particle);
+            crc2.restore();
+        }
+        crc2.restore();
+    }
+
+    function drawSun(_position: Vector): void {
+        console.log("sun");
+
+        let r1: number = 30;
+        let r2: number = 150;
+        let gradient: CanvasGradient = crc2.createRadialGradient(0, 0, r1, 0, 0, r2);
+
+        gradient.addColorStop(0, "HSLA(60, 100%, 95%, 1)");
+        gradient.addColorStop(1, "HSLA(60, 100%, 100%, 0)");
+
+        crc2.save();
+        crc2.translate(_position.x, _position.y);
+        crc2.fillStyle = gradient;
+        crc2.arc(0, 0, r2, 0, 2 * Math.PI);
+        crc2.fill();
+        crc2.restore();
+    }
+
+    //inspired by Gina 
+    function drawDaisy(): void {
+        console.log("flowers");
+
+        let x: number = (Math.random() * canvas.width - 10);
+        let y: number = (Math.random() * (canvas.height - canvas.height * 0.5) + canvas.height * 0.5);
+
+        crc2.save();
+
+        crc2.beginPath();
+        crc2.moveTo(x, y);
+        crc2.translate(x, y);
+        crc2.quadraticCurveTo(10, 5, 10, 30);
+        crc2.strokeStyle = "#103517";
+
+        crc2.stroke();
+
+        crc2.beginPath();
+        moveTo(10, 20);
+        crc2.arc(0, 0, 6, 0, 2 * Math.PI);
+        crc2.fillStyle = "#FCF238";
+        crc2.strokeStyle = "##FCF238";
+        crc2.fill();
+        crc2.stroke();
+
+        for (let blossoms: number = 80; blossoms > 8; blossoms -= 8) {
+
+            crc2.beginPath();
+            moveTo(10, 20);
+            crc2.rotate(45 * Math.PI / 20);
+            crc2.arc(10, 0, 5, 0, 2 * Math.PI);
+            crc2.fillStyle = "#FD9300";
+            crc2.strokeStyle = "#FD9300";
+            crc2.fill();
+
+            crc2.stroke();
+        }
+
+        crc2.restore();
+    }
+
+    function drawTulip(): void {
+
+        let x: number = (Math.random() * canvas.width - 10);
+        let y: number = (Math.random() * (canvas.height - canvas.height * 0.5) + canvas.height * 0.5);
+
+        crc2.save();
+
+        crc2.beginPath();
+        crc2.moveTo(x, y);
+        crc2.translate(x, y);
+        crc2.lineTo(0, 30);
+        crc2.strokeStyle = "#358443";
+
+        crc2.stroke();
+
+        crc2.beginPath();
+        moveTo(10, 20);
+        crc2.arc(0, 0, 9, 0, 1 * Math.PI);
+        
+        crc2.fillStyle = "#BB0808";
+        crc2.strokeStyle = "##BB0808";
+        crc2.fill();
+        crc2.stroke();
+        moveTo(0, 20);
+        crc2.lineTo(-10, -10);
+        crc2.lineTo(-3, 2);
+        crc2.lineTo(1, -10);
+        crc2.lineTo(4, 2);
+        crc2.lineTo(9, -10);
+        crc2.lineTo(9, 3);
+        crc2.closePath();
+        crc2.fillStyle = "#BB0808";
+        crc2.fill();
+        crc2.stroke();
+
+        crc2.restore();
+    
+    }
+
+    function drawFlowers(): void {
+
+        for (let i: number = 0; i < 10; i++) {
+            drawDaisy();
+            
+        }
+
+        for (let i: number = 0; i < 10; i++) {
+            drawTulip();
+            
+        }
+
+    }
+
+}
